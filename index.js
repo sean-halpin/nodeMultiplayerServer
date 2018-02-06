2d6952e3ca8419cbb5b164018e844c8798b62876
Sean Halpin <softdev87@users.noreply.github.com>
2018-02-06 21:38:28 +0000

    Server Socket Emit Throttling
    code cleanup
    Simple UI

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 const express = require('express');
 var app = express();
 var server = require('http').Server(app);
-// Loading socket.io
 var io = require('socket.io').listen(server);
 
 var HashMap = require('hashmap');
 var gameState = new HashMap();
+var lastFrameRenderTime = new Date().getTime();
 
 io.sockets.on('connection', function (socket, username) {
-    // When the client connects, they are sent a message
     socket.emit('message', 'You are connected!');
-    // The other clients are told that someone new has arrived
     socket.broadcast.emit('message', 'Another client has just connected!');
-
-    // As soon as the username is received, it's stored as a session variable
     socket.on('new_user', function(username) {
         socket.username = username;
     });
 
-    // When a "message" is received (click on the button), it's logged in the console
     socket.on('message', function (message) {
-        // The username of the person who clicked is retrieved from the session variables
         console.log('User:' + socket.username + ' Message:' + message);
     }); 
 
     socket.on('client_update', function (message) {
-        console.log('User:' + socket.username + ' Message:' + message);
+        //console.log('User:' + socket.username + ' Message:' + message);
         var payload = JSON.parse(message);
         if(!gameState.has(payload.player)){
           var playerState = 
@@ -57,8 +51,11 @@ io.sockets.on('connection', function (socket, username) {
                console.log('Unknown Direction Recieved');
         }
         gameState.set(payload.player, playerState);
-        console.log("Return Payload : ", gameState.values());
-        socket.emit('game_state_update', gameState.values());
+        var thisFrameRenderTime = new Date().getTime();
+        if(thisFrameRenderTime-lastFrameRenderTime > (1000/33)){
+              //console.log("Return Payload : ", gameState.values());
+              socket.emit('game_state_update', gameState.values());
+        }
     })
 });
 
--- public/game.js
+++ public/game.js
@@ -1,20 +1,16 @@
+var username = "default";
 var socket = io.connect();
-// The visitor is asked for their username...
-var username = prompt('What is your username?');
-// It's sent with the signal "new_user" (to differentiate it from "message")
-socket.emit('new_user', username);
-// A dialog box is displayed when the server sends us a "message"
 socket.on('message', function(message) {
-    alert('Server response: ' + message);
+    console.log('Server message: ' + message);
 })
-//
 socket.on('game_state_update', function(message) {
-    drawMultiplePlayers(message);
+    gameArea.drawFrame(message);
 })
 
-// When the button is clicked, a "message" is sent to the server
-$('#ping').click(function () {
-    socket.emit('message', 'Client Ping Test');
+$('#join').click(function () {
+	username = prompt('What is your username?');
+	socket.emit('new_user', username);
+	sendCommand("up");
 })
 
 var up = new Image;
@@ -26,74 +22,70 @@ left.src = "left.png";
 var right= new Image;
 right.src = "right.png";
 
-var date = new Date();
-var n = date.getTime();
-
-var w = window.innerWidth;
-var h = window.innerHeight;
-var arena = playArea();
-
-function playArea(){
-	var canvas = document.createElement('canvas');
-	canvas.style.display = "block";
-	canvas.id = "playArea";
-	canvas.width = w;
-	canvas.height = h;
-	document.body.appendChild(canvas);
-	document.body.style.margin = "0";
-	return canvas;
-}
-
-function drawMultiplePlayers(json){
-	var ctx = arena.getContext("2d");
-	ctx.save(); 
-	ctx.clearRect(0, 0, w, h);
-	ctx.fillStyle = "rgb(65, 191, 74)";
-	ctx.fillRect(0, 0, arena.width, arena.height);
-	
-	for(var i=0;i<json.length;i++){
-		var X = 8*(json[i].x) + w/2;
-		var Y = 8*(json[i].y) + h/2;
-		
-		switch (json[i].direction){
-			case "up":
-				ctx.drawImage(up,X-(up.width*0.4/2),Y-(up.height*0.4/2),(up.width*0.4),(up.height*0.4));
-				break;
-			case "down":
-				ctx.drawImage(down,X-(down.width*0.4/2),Y-(down.height*0.4/2),(down.width*0.4),(down.height*0.4));
-				break;
-			case "left":
-				ctx.drawImage(left,X-(left.width*0.4/2),Y-(left.height*0.4/2),(left.width*0.4),(left.height*0.4));
-				break;
-			case "right":
-				ctx.drawImage(right,X-(right.width*0.4/2),Y-(right.height*0.4/2),(right.width*0.4),(right.height*0.4));
-				break;
-			default: 
-			console.log('Unknown Direction Recieved');
+var lastFrameRenderTime = new Date().getTime();
+var gameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight - ($('#joinButton').height() * 3.2);
+        this.context = this.canvas.getContext("2d");
+        this.canvas.style.display = "block";
+        document.body.style.margin = "20";
+        document.body.appendChild(this.canvas, document.body.childNodes[0]);
+        },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    drawFrame : function(json) {
+    	this.context.save();
+		this.clear();
+		this.context.fillStyle = "rgb(65, 191, 74)";
+		this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+		for(var i=0;i<json.length;i++){
+			var X = 8*(json[i].x) + this.canvas.width/2;
+			var Y = 8*(json[i].y) + this.canvas.height/2;
+			
+			switch (json[i].direction){
+				case "up":
+					this.context.drawImage(up,X-(up.width*0.4/2),Y-(up.height*0.4/2),(up.width*0.4),(up.height*0.4));
+					break;
+				case "down":
+					this.context.drawImage(down,X-(down.width*0.4/2),Y-(down.height*0.4/2),(down.width*0.4),(down.height*0.4));
+					break;
+				case "left":
+					this.context.drawImage(left,X-(left.width*0.4/2),Y-(left.height*0.4/2),(left.width*0.4),(left.height*0.4));
+					break;
+				case "right":
+					this.context.drawImage(right,X-(right.width*0.4/2),Y-(right.height*0.4/2),(right.width*0.4),(right.height*0.4));
+					break;
+				default: 
+					console.log('Unknown Direction Recieved');
+			}
 		}
-	}
-	ctx.restore(); 
+		this.context.restore(); 
+    }
 }
 
-function postPlay(dir){
-	var date2 = new Date();
-	var nplus = date2.getTime();
-	if(nplus-n > (1000/10)){
-	var data = JSON.stringify({"player": username, "direction": dir});
-	socket.emit('client_update', data);;
-	n = nplus;
+gameArea.start();
+
+function sendCommand(dir){
+	var thisFrameRenderTime = new Date().getTime();
+	if(thisFrameRenderTime-lastFrameRenderTime > (1000/33)){
+		var data = JSON.stringify({"player": username, "direction": dir});
+		socket.emit('client_update', data);;
+		lastFrameRenderTime = thisFrameRenderTime;
 	}
 }
 
 window.onkeydown = function (e) {
     var code = e.keyCode ? e.keyCode : e.which;
     if (code === 87) { //up key w
-        postPlay("up");
+        sendCommand("up");
     } else if (code === 83) { //down key s
-        postPlay("down");
+        sendCommand("down");
     } else if (code === 65) { //left key a
-        postPlay("left");
+        sendCommand("left");
     } else if (code === 68) { //right key d
-        postPlay("right");
+        sendCommand("right");
     }
 };
